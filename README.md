a481d7abab4b1998809bfac388634a87f6762bda
Aashish1-1-1 <aashishadhikari693@gmail.com>
2024-09-30 15:46:42 +0545

    [Added:functional version]

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # lyrics.nvim
-A nvim plugin to get lyrics of song you're listening to...
+A nvim plugin to get lyrics of song you're listening to.
+
+# Why??
+Are you listening to a song while writing some crappy code and want to sing it but don't know the lyrics, I got you bro. Presenting lyrics.nvim which gurantees to provide lyrics to song that is listed in genius
+
+# To use 
+Just add following code to you configfile
+## If lazy
+```lua
+{
+    "Aashish1-1-1/lyrics.nvim"
+}
+```
--- lua/init.lua
+++ lua/init.lua
@@ -1,18 +1,50 @@
 local M = {}
-
+local curl = require("plenary.curl")
+function print_table(t)
+    for k, v in pairs(t) do
+        if type(v) == "table" then
+            print(k .. ": ")
+        else
+            print(k .. ": " .. tostring(v))
+        end
+    end
+end
+local function split_string_by_newlines(str)
+    local result = {}
+    for line in str:gmatch("([^\n]*)\n?") do
+        table.insert(result, line)
+    end
+    return result
+end
 function M.getlyrics(song)
     local buf = vim.api.nvim_create_buf(false, true)
     vim.api.nvim_buf_set_option(buf,'bufhidden','wipe')
-   
+  
     vim.api.nvim_open_win(buf, true, {
         relative = 'editor',
         width = 30,
-        height = 10,
-        row = 18,
-        col = 0,
+        height = 30,
+        row = 0,
+        col = 100,
         border = 'rounded',
     })
-    vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "Hello, welcome to my plugin!","This is line2 ","This is line 3" })
+    --preparing for curl
+    local lyrics = ""
+    local query = tostring(song)
+    query = query:gsub("%s+", "/")  
+    local url = "https://lyrist.vercel.app/api/" .. query
+    local response = curl.get(url,{
+        timeout = 5000
+      })
+      if response.status == 200 then
+        data = vim.json.decode(response.body)
+        --print(data.lyrics)
+        --print(type(data.lyrics)) 
+          lyrics = split_string_by_newlines(data.lyrics)
+      else
+        print("Error: " .. response.status .. " - " .. response.body)
+      end
+      vim.api.nvim_buf_set_lines(buf, 0, -1, false,lyrics)
 end
 
 vim.api.nvim_create_user_command(
@@ -22,6 +54,4 @@ vim.api.nvim_create_user_command(
     end,
     {nargs = 1}
 )
-
 return M
-
